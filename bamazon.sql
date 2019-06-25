71c75f7eb2f99a69e649a9855f79f91f5e74790c
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-24 19:38:52 -0700

    Creating products table within bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -3,3 +3,12 @@ DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 
 USE bamazon;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50),
+    department_name VARCHAR(50),
+    price INT,
+    stock_quantity INT,
+    PRIMARY KEY (item_id)
+);
