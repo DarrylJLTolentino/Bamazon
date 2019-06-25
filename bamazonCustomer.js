78a411ad4fe9d7d5bb8decf04fc0fcbe0bb502e1
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-25 14:27:44 -0700

    Adding showAll function that displays table in terminal within bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,6 +22,23 @@ connection.connect(function (err) {
     // menu();
 });
 
+function showAll() {
+    console.log("Showing all products in Bamazon...\n");
+    // console.log("item_id  product_name                          department_name  price  stock_quantity");
+    // console.log("-------  ------------------------------------  ---------------  -----  --------------");
+    var table = new Table({
+        head: ["item_id", "product_name", "department_name", "price", "stock_quantity"],
+        colWidths: [15, 25, 25, 10, 20]
+    });
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
+        console.log(table.toString());
+    });
+}
+
 // function menu() {
 //     inquirer.prompt([
 //         {
