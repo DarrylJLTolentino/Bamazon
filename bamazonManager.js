032d6ff3a9a83f736827c5b2124f8741fc694f2c
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-26 11:53:14 -0700

    Adding Ask Manager function to bamazonManager.js

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-var inquirer = requirer("inquirer");
+var inquirer = require("inquirer");
 var Table = require("cli-table");
 
 var connection = mysql.createConnection({
@@ -18,4 +18,35 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+    AskManager();
 });
+
+function AskManager() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do, Manager?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
+        }
+    ]).then(function(response) {
+        switch (response.choice) {
+            case "View Products for Sale":
+                ViewProductsOnSale();
+                break;
+            case "View Low Inventory":
+                ViewLowInventory();
+                break;
+            case "Add to Inventory":
+                AddInventory();
+                break;
+            case "Add New Product":
+                AddNewProduct();
+                break;
+            case "Exit":
+                console.log("Terminating link...\n");
+                connection.end();
+                break;
+        }
+    })
+}
