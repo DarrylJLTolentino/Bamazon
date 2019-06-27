337c1e8b812ebb00a43a1fd27db051c9f143e803
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-27 00:18:00 -0700

    adding AskSupervisor function within bamazonSupervisor.js

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -18,4 +18,34 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+    AskSuperVisor();
 });
+
+function AskSuperVisor() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do, Supervisor?",
+            choices: ["View Product Sales by Department", "Create New Department", "Exit"]
+        }
+    ]).then(function(response) {
+        switch (response.choice) {
+            case "View Product Sales by Department":
+                ViewProductSales();
+                break;
+            case "Create New Department":
+                CreateNewDepartment();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+        }
+    })
+}
+
+//ViewProductSales()
+//Going to be a Join of all columns from department table
+//and product_sales from products table.
+//total_profit is difference between over_head_costs and product_sales.
+//total_profit is a custom alias (AS total_profit)
